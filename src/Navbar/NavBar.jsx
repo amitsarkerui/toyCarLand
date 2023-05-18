2da656380bcc3dd3c4be0c41b4cc15c15c379025
Amit Sarker <amitsarkerstock@gmail.com>
2023-05-19 01:32:08 +0600

    client site setup

---
+++ src/Navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/logo/logo.svg";
+
+const NavBar = () => {
+  return (
+    <div>
+      <img className="max-w-[300px]" src={logo} alt="" />
+    </div>
+  );
+};
+
+export default NavBar;
