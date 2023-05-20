22c4d9373f3e38224508034a2534b990e432d55f
Amit Sarker <amitsarkerstock@gmail.com>
2023-05-20 23:02:38 +0600

    search feild no result issue fixed

--- src/Alltoys/AllToys.jsx
+++ src/Alltoys/AllToys.jsx
@@ -60,58 +60,75 @@ const AllToys = () => {
           </button>
         </div>
       </form>
-
-      <table className="table w-full">
-        {/* head */}
-        <thead>
-          <tr>
-            <th>Toys Name</th>
-            <th>Seller</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th></th>
-          </tr>
-        </thead>
-        {allToys.map((toy) => (
-          <tbody key={toy._id}>
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-28 h-28">
-                      <img
-                        src={toy.photo}
-                        alt="Avatar Tailwind CSS Component"
-                      />
+      {allToys.length === 0 ? (
+        <div className="my-4">
+          <img
+            className="text-center mx-auto"
+            src="https://i.ibb.co/wKwT9wG/Sorry.png"
+            alt=""
+          />
+          <h3 className="text-2xl text-center">
+            Sorry ! Can't find any result
+          </h3>
+        </div>
+      ) : (
+        <>
+          {" "}
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Toys Name</th>
+                <th>Seller</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th></th>
+              </tr>
+            </thead>
+            {allToys.map((toy) => (
+              <tbody key={toy._id}>
+                <tr>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-28 h-28">
+                          <img
+                            src={toy.photo}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <h4 className="">{toy.name}</h4>
+                        <div className="text-sm mt-2">
+                          <span className="bg-[#ECCC68] px-2 py-[2px] rounded-lg text-gray-900">
+                            {toy.category}
+                          </span>{" "}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <h4 className="">{toy.name}</h4>
-                    <div className="text-sm mt-2">
-                      <span className="bg-[#ECCC68] px-2 py-[2px] rounded-lg text-gray-900">
-                        {toy.category}
-                      </span>{" "}
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                {toy.seller}
-                <br />
-                <span className="badge badge-ghost badge-sm">{toy.email}</span>
-              </td>
+                  </td>
+                  <td>
+                    {toy.seller}
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      {toy.email}
+                    </span>
+                  </td>
 
-              <td>$ {toy.price}</td>
-              <td>{toy.quantity}</td>
-              <th>
-                <button className="btn bg-[#ECCC68] border-none text-gray-900 hover:text-white">
-                  details
-                </button>
-              </th>
-            </tr>
-          </tbody>
-        ))}
-      </table>
+                  <td>$ {toy.price}</td>
+                  <td>{toy.quantity}</td>
+                  <th>
+                    <button className="btn bg-[#ECCC68] border-none text-gray-900 hover:text-white">
+                      details
+                    </button>
+                  </th>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </>
+      )}
     </div>
   );
 };
