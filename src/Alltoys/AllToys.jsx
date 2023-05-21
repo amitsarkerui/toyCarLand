1b0559fef4aea7b950d7d2957b93951638bab69e
Amit Sarker <amitsarkerstock@gmail.com>
2023-05-21 14:55:11 +0600

    sort by price

--- src/Alltoys/AllToys.jsx
+++ src/Alltoys/AllToys.jsx
@@ -28,52 +28,75 @@ const AllToys = () => {
       });
     }
   };
-
+  const sortToysByPriceLowToHigh = () => {
+    const sortedToys = [...allToys].sort((a, b) => a.price - b.price);
+    setAllToys(sortedToys);
+  };
+  const sortToysByPriceHighToLow = () => {
+    const sortedToys = [...allToys].sort((a, b) => b.price - a.price);
+    setAllToys(sortedToys);
+  };
   return (
     <div className="overflow-x-auto w-full py-16">
       <h2 className="text-2xl text-center mb-8">All Toys</h2>
-
-      <form onSubmit={handleSearch} className="w-1/3 mx-auto mb-10">
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search
-        </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-          </div>
-          <input
-            type="search"
-            name="searchBox"
-            id="default-search"
-            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search cars by name..."
-            required
-          />
-          <button
-            type="submit"
-            className="text-white absolute right-2.5 bottom-2.5 bg-[#ECCC68] hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-[#ECCC68] dark:hover:grey-blue-700 dark:focus:ring-grey-800"
+      <div className="flex gap-8 justify-between">
+        <form onSubmit={handleSearch} className="w-1/3 mb-10 ">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
             Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              name="searchBox"
+              id="default-search"
+              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search cars by name..."
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-[#ECCC68] hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-grey-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-[#ECCC68] dark:hover:grey-blue-700 dark:focus:ring-grey-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+        <div>
+          <button
+            onClick={sortToysByPriceLowToHigh}
+            className="btn bg-[#ECCC68] border-none text-gray-900"
+          >
+            $ Low to High
+          </button>
+          <button
+            onClick={sortToysByPriceHighToLow}
+            className="btn bg-[#ECCC68] border-none text-gray-900 ml-2"
+          >
+            $ High to Low
           </button>
         </div>
-      </form>
+      </div>
+
       {allToys.length === 0 ? (
         <div className="my-4">
           <img
